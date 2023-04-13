46e637a8d0bba7d63a8fed1d34fc0836fc27443b
Khanhphan98 <khanhphan9898@gmail.com>
2023-04-13 16:27:39 +0700

    xoá file

--- src/model/interface/IIpAddress.ts
+++
@@ -1,8 +0,0 @@
-export interface IIpAddress {
-  id: string;
-  ip: string;
-  reason: string;
-  deletable: boolean;
-  is_primary: boolean;
-  create_at: Date;
-}
--- src/model/interface/IIpGroup.ts
+++
@@ -1,7 +0,0 @@
-export interface IIpGroup {
-  id: string;
-  name: string;
-  ips: string[];
-  create_at: Date;
-  updated_at: Date;
-}
